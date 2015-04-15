18857026c2bfa436c493266ef409b1fa801e6a78
Tommy Murphy <tmurphy@shopkeep.com>
2015-04-14 22:41:02 -0400

    Update sslyze to 0.11.0 release

--- sslyze.rb
+++ sslyze.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sslyze < Formula
   homepage 'https://github.com/nabla-c0d3/sslyze'
-  url 'https://github.com/nabla-c0d3/sslyze/archive/c49f96813200701782e2e602f3792b778417d845.tar.gz'
-  sha256 '21f9f5e6c01f3c871b56b74eb8b8651d063c354f8425b37d67402a9c2755f43b'
+  url 'https://github.com/nabla-c0d3/sslyze/archive/release-0.11.tar.gz'
+  sha256 'd0adf9be09d5b27803a923dfabd459c84a2eddb457dac2418f1bf074153f8f93'
   version '0.11.0'
 
   depends_on :arch => :x86_64
@@ -12,13 +12,13 @@ class Sslyze < Formula
   conflicts_with 'openssl', :because => 'nassl builds against its own openssl'
 
   resource 'nassl' do
-    url 'https://github.com/nabla-c0d3/nassl/archive/9a5f0f1ad5bc1b12c8d69acbc4e1c0d975e41a19.tar.gz'
-    sha256 'f363fe170e57343c2a5ac93a3eefdf6bd96268442dd4cdffe1027abf995eb974'
+    url 'https://github.com/nabla-c0d3/nassl/archive/v0.11.tar.gz'
+    sha256 '83fe1623ad3e67ba01a3e692211e9fde15c6388f5f3d92bd5c0423d4e9e79391'
   end
 
   resource 'openssl' do
-    url 'https://www.openssl.org/source/old/1.0.1/openssl-1.0.1i.tar.gz'
-    sha256 '3c179f46ca77069a6a0bac70212a9b3b838b2f66129cb52d568837fc79d8fcc7'
+    url 'https://www.openssl.org/source/openssl-1.0.2a.tar.gz'
+    sha256 '15b6393c20030aab02c8e2fe0243cb1d1d18062f6c095d67bca91871dc7f324a'
   end
 
   resource 'zlib' do
@@ -39,7 +39,7 @@ class Sslyze < Formula
       nasslpath = Pathname.new(Dir.pwd)
 
       resource('openssl').stage do
-        (nasslpath/'openssl-1.0.1i').install Dir["*"]
+        (nasslpath/'openssl-1.0.2a').install Dir["*"]
       end
 
       resource('zlib').stage do
